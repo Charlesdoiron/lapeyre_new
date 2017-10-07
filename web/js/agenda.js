da8cf840ae67a5fca2182f5bf3d2c2c3169e7a48
Charles d_Oiron <doironcharles@gmail.com>
2017-10-07 11:12:37 +0200

    Total + Agenda en cours + Panier quasi finalisé

--- web/js/agenda.js
+++ web/js/agenda.js
@@ -1,12 +1,54 @@
-function cellIsActive() {
-var cells = $('.cell__hour')
+$(document).ready(function() {
 
-for (var i = cells.length - 1; i >= 0; i--) {
-	var cell = cells[i]
-	cell.classList.toggle('cell__is-active');
-}
-	
- };
+    $('.calendar').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    })
 
 
+    // 	var hours = $(".calendar__hour");
 
+    // 	$('.calendar__hour').addEventListener('click',function () {
+    // 		console.log(hello);
+    // 	})
+
+    // console.log(hours);
+
+
+    var hours = document.getElementsByClassName('calendar__hour');
+  
+    // for (var i = Things.length - 1; i >= 0; i--) {
+    // 	Things[i]
+    // }
+
+
+    $(hours).click(function() {
+
+
+        for (var i = hours.length - 1; i >= 0; i--) {
+            var hour = hours[i]
+                                 console.log(hour[i]);;
+
+        }
+
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// AJOUTER LA CLASSE ACTIVE A L'HEURE SELECTIONNEE
